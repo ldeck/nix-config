f4fb8b46150d1b5e775fcfaa78012b90b2b02d0c
Lachlan Deck <lachlan.deck@gmail.com>
2020-01-03 03:55:29 +1100

    add config.nix with my-packages

---
+++ nixpkgs/config.nix
@@ -0,0 +1,38 @@
+{
+  allowUnfree = true;
+  packageOverrides = pkgs: with pkgs; rec {
+
+    myProfile = pkgs.writeText "my-profile" ''
+      export PATH=$HOME/.nix-profile/bin:/nix/var/nix/profiles/default/bin:/sbin:/bin:/usr/sbin:/usr/bin
+      export MANPATH=$HOME/.nix-profile/share/man:/nix/var/nix/profiles/default/share/man:/usr/share/man
+    '';
+    
+    myPackages = with pkgs; buildEnv {
+      name = "my-packages";
+      paths = [
+        (runCommand "profile" {} ''
+          mkdir -p $out/etc/profile.d
+          cp ${myProfile} $out/etc/profile.d/my-profile.sh
+        '')
+	aspell
+	bc
+	coreutils
+	direnv
+	emacs
+	emscripten
+	ffmpeg
+	gdb
+	git
+	hello
+	jdk11
+	jq
+	nixops
+	nox
+	scala
+	silver-searcher
+      ];
+      pathsToLink = [ "/share/man" "/share/doc" "/bin" "/etc" "/Applications" ];
+      extraOutputsToInstall = [ "man" "doc" ];
+    };
+  };
+}
