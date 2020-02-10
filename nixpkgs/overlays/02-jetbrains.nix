4dbfe0aa600bfd07c2f42766690ca27256c5074c
Lachlan Deck <lachlan.deck@gmail.com>
2020-02-10 21:44:46 +1100

    updates: idea-ultimate => 2019.3.2, jetbrains.sdk to 11_0_6...b702.1

--- nixpkgs/overlays/02-jetbrains.nix
+++ nixpkgs/overlays/02-jetbrains.nix
@@ -2,20 +2,20 @@ self: super:
 {
   jetbrains = super.jetbrains // {
     jdk = super.jetbrains.jdk.overrideAttrs (oldAttrs: rec {
-      version = "520.11";
+      version = "11_0_6-osx-x64-b702.1";
       src = super.fetchurl {
-        url = "https://bintray.com/jetbrains/intellij-jbr/download_file?file_path=jbrsdk-11_0_4-osx-x64-b520.11.tar.gz";
-  	sha256 = "3fe1297133440a9056602d78d7987f9215139165bd7747b3303022a6f5e23834";
+        url = "https://bintray.com/jetbrains/intellij-jbr/download_file?file_path=jbrsdk-${version}.tar.gz";
+  	sha256 = "1ra33mp71awhmzf735dq7hxmx9gffsqj9cdp51k5xdmnmb66g12s";
       };
       passthru = oldAttrs.passthru // {
         home = "${self.jetbrains.jdk}/Contents/Home";
       };
     });
     idea-ultimate = super.jetbrains.idea-ultimate.overrideAttrs (_: {
-      name = "idea-ultimate.2019.2.4";
+      name = "idea-ultimate.2019.3.2";
       src = super.fetchurl {
-        url = "https://download.jetbrains.com/idea/ideaIU-2019.2.4-no-jbr.tar.gz";
-	sha256 = "09mz4dx3zbnqw0vh4iqr8sn2s8mvgr7zvn4k7kqivsiv8f79g90a";
+        url = "https://download.jetbrains.com/idea/ideaIU-2019.3.2-no-jbr.tar.gz";
+	sha256 = "09lgdd7gkx94warjc7wah9w7s9lj81law8clavjjyjas8bhhf1hz";
       };
     });
   };
