d57e5ce629d70d125d4ea6163d43ceaadab475fa
Lachlan Deck <lachlan.deck@gmail.com>
2020-10-20 04:07:46 +1100

    update Firefox to 81.0.2

--- nixpkgs/overlays/30-macos-apps.nix
+++ nixpkgs/overlays/30-macos-apps.nix
@@ -51,12 +51,12 @@ Docker = self.installApplication rec {
 
 Firefox = self.installApplication rec {
   name = "Firefox";
-  version = "80.0.1";
+  version = "81.0.2";
   sourceRoot = "Firefox.app";
   src = super.fetchurl {
     name = "Firefox-${version}.dmg";
     url = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/${version}/mac/en-US/Firefox+${version}.dmg";
-    sha256 = "1l3gkkmxbgzjn72ncvbpk2br2j08sgvzrmlgcvv0hdn9rwnbnb18";
+    sha256 = "1khzsnrk13z2lrwqd1vc6jm3qg7vmrgy996ss61ybfb6prjjj55s";
   };
   postInstall = ''
     mkdir -p $out/bin
