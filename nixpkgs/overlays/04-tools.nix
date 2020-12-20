de76b13895bc1fc2be7d4218c6f1669ead6a0613
Lachlan Deck <lachlan.deck@gmail.com>
2020-12-21 06:45:47 +1100

    remove lzfse overlay now that it's supporting all platforms

--- nixpkgs/overlays/04-tools.nix
+++ nixpkgs/overlays/04-tools.nix
@@ -2,9 +2,4 @@ self: super: {
   emscriptenVersion = "1.39.20";
 
 
-  lzfse = super.lzfse.overrideAttrs(oldAttrs: rec {
-    meta = oldAttrs.meta // {
-      platforms = with super.stdenv.lib; platforms.all;
-    };
-  });
 }
