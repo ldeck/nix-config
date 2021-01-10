3bbfc2d8114d83cf01467bfe08996e46604b3ddf
Lachlan Deck <lachlan.deck@gmail.com>
2021-01-10 15:39:52 +1100

    update Zotero to 5.0.94

--- nixpkgs/overlays/30-macos-apps.nix
+++ nixpkgs/overlays/30-macos-apps.nix
@@ -222,18 +222,19 @@ TibcoJaspersoftStudio = self.installApplication rec {
 
 Zotero = self.installApplication rec {
   name = "Zotero";
-  version = "5.0.93";
+  version = "5.0.94";
   sourceRoot = "Zotero.app";
   src = super.fetchurl {
     name = "zotero-${version}.dmg";
     url = "https://download.zotero.org/client/release/${version}/Zotero-${version}.dmg";
-    sha256 = "0hg0xs41wx4ncwbps22bgga0hlgdiaw0dmb11iljvyn0xp6cx63r";
+    sha256 = "1chfgmx4mjbxqb7fwdmgy70kwm0z78w5x1vih85qf083byz7qclv";
   };
   description = ''
     Zotero is a free, easy-to-use tool to help you collect, organize, cite,
     and share your research sources
   '';
   homepage = https://www.zotero.org;
+  appcast = https://www.zotero.org/download/;
 };
 
 }
