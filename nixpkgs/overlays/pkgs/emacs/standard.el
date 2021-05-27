af6d24681c91ac260acae507b09b91343879c852
Lachlan Deck <lachlan.deck@gmail.com>
2021-05-28 03:53:49 +1000

    emacs: improve helm key-bindings (especially for kill-ring)
    Borrowing inspiration from prelude:
    https://github.com/bbatsov/prelude/blob/aa22e84eea7cb9e9b7e6998162bb302a99eed3df/modules/prelude-helm-everywhere.el

--- nixpkgs/overlays/pkgs/emacs/standard.el
+++ nixpkgs/overlays/pkgs/emacs/standard.el
@@ -129,16 +129,28 @@
 
 (use-package helm
   :config
-  ;;(helm-mode 1)
   (require 'helm-config)
-
-
   (global-set-key (kbd "M-x") 'helm-M-x)
+  (global-set-key (kbd "M-y") 'helm-show-kill-ring)
   (global-set-key (kbd "C-h f") 'helm-apropos)
-  (global-set-key (kbd "C-x C-f") 'helm-find-files)
+  (global-set-key (kbd "C-h r") 'helm-info-emacs)
   (global-set-key (kbd "C-x b") 'helm-mini)
-  (global-set-key (kbd "C-c y") 'helm-show-kill-ring)
-  (global-set-key (kbd "C-x C-r") 'helm-recentf))
+  (global-set-key (kbd "C-x f") 'helm-recentf)
+  (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+  (global-set-key (kbd "C-x C-f") 'helm-find-files)
+  (global-set-key (kbd "C-x C-l") 'helm-locate-library)
+  (global-set-key (kbd "C-x C-r") 'helm-recentf)
+
+  (define-key minibuffer-local-map (kbd "C-c C-l") 'helm-minibuffer-history)
+  ;;(define-key isearch-mode-map (kbd "C-o") 'helm-occur-from-isearch)
+  ;; (define-key shell-mode-map (kbd "C-c C-l") 'helm-comint-input-ring)
+
+  ;; use helm to list eshell history
+  (add-hook 'eshell-mode-hook
+            #'(lambda ()
+                (substitute-key-definition 'eshell-list-history 'helm-eshell-history eshell-mode-map)))
+  (substitute-key-definition 'find-tag 'helm-etags-select global-map)
+  (helm-mode 1))
 
 (use-package helm-descbinds
   :after (helm)
